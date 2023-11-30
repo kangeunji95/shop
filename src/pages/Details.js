5c7941bd99b434dfd536a971483070054cd46186
kangeunji95 <kangeunji95dmswl3597@gmail.com>
2023-11-30 10:39:33 +0900

    first_commit ver.01

---
+++ src/pages/Details.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useParams } from 'react-router'
+
+export default function Details(props) {
+
+  const {bests} =props
+  const {id} = useParams()
+  return (
+    <div>
+      <h2>Details Page</h2>
+      <img src={bests[id].image} alt="" />
+      <h4>{bests[id].title}</h4>
+      <p>{bests[id].desc}</p>
+      <p>{bests[id].price}</p>
+    </div>
+  )
+}
